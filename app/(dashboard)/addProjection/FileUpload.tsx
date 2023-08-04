cda000ab5ede24a5c332a520fdd3f809dbb19109
Kalash Jain <kalashjain54@gmail.com>
2023-08-04 15:55:21 +0530

    files

--- app/(dashboard)/addProjection/FileUpload.tsx
+++ app/(dashboard)/addProjection/FileUpload.tsx
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import { BsFolderPlus } from "react-icons/bs";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
+
+import { storage } from "@/firebase";
+
 
 function FileUpload() {
   const [file, setFile] = useState(null);
   // console.log(e)
   const uploadFile = () => {};
+  const onChange =(event:React.ChangeEvent<HTMLInputElement>) => {
+    const storageRef = ref(storage, event.target.value);
+
+    if(! event.target.files?.[0] ) {return};
+    uploadBytes(storageRef,event.target.files[0]).then((snapshot) => {
+      console.log('Uploaded an array!');
+    });
+  }
 
   return (
     <div className="flex-col  justify-center">
@@ -13,9 +25,7 @@ function FileUpload() {
       <input
         className="bg-[#10776b] p-2 rounded-md text-white text-sm"
         type="file"
-        onChange={(event) => {
-          console.log(event.target.value);
-        }}
+        onChange={onChange}
       />
     </div>
   );
