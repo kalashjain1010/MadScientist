1c82728d9f3a08750f7321fcdf577b6341f59a00
Kalash Jain <kalashjain54@gmail.com>
2023-08-04 18:18:41 +0530

    project page

--- app/(dashboard)/addProjection/addProjectionClient.tsx
+++ app/(dashboard)/addProjection/addProjectionClient.tsx
@@ -76,7 +76,16 @@ const AddProjectionClient = () => {
             </div>
             <div className="group-hover:opacity-70">What is projection ?</div>
           </div>
-          <div>{/* display content here in this div noob */}</div>
+          <div className="p-6">
+            <p className="mb-2">Files / Media uploaded to MadScientist are referred to as "Projections" . A Projection can be anything you wish to upload on the MadScientist platform to show your interest in Science & Technology.</p>
+            <p className="mt-8">The following are the file types & sizes a Projection can accept :</p>
+            <p className="mt-5">rightPointingHandImage : PNG, JPEG/JPG, GIF - Max file size - 7 MB</p>
+            <p>Video : MP4, MOV, MPEG, WEBM - Max file size - 60 MB</p>
+            <p>Audio : MP3, AAC - Max file size - 10 MB</p>
+            <p>Document: PDF - Max file size - 12 MB</p>
+            <p>3D : GLB , GLTF (For a better user experience, please convert your other type 3D files into canvas ready GLTF/GLB 3D files and upload them to MadScientist ) - Max file size - 100 MB</p>
+            <p className=" mt-24 ">Note: You can upload one type of projection at a time. We are trying to improve this.</p>
+          </div>
         </div>
         <div
           className={`border-r-2 border-slate-300/80 p-6 ${
